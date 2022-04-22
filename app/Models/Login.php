ea2f2d42e9b309f9f3106edc5a0be5cf32db709c
Fathur Walkers <fathurwalkers44@gmail.com>
2022-04-22 11:14:47 -0700

    update

--- app/Models/Login.php
+++ app/Models/Login.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Hasil;
 
 class Login extends Model
 {
     use HasFactory;
+    protected $table = 'login';
+    protected $guarded = [];
+    protected $primaryKey = 'id';
+
+    public function hasil()
+    {
+        return $this->hasMany(Hasil::class);
+    }
 }
--- database/migrations/2022_04_22_061210_create_hasils_table.php
+++ database/migrations/2022_04_22_061210_create_hasils_table.php
@@ -6,24 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateHasilsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('hasil', function (Blueprint $table) {
             $table->id();
+            $table->string('hasil_kode');
+            $table->string('hasil_nama_pengguna');
+            $table->string('hasil_total_nilai');
+            $table->dateTime('hasil_waktu');
+            $table->string('keterangan');
+
+            $table->unsignedBigInteger('login_id')->nullable()->default(null);
+            $table->foreign('login_id')->references('id')->on('login')->onDelete('cascade');
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('hasil');
