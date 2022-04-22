b05357400c599dc3ed72d7b8b1e29594232f2d69
Fathur Walkers <fathurwalkers44@gmail.com>
2022-04-22 11:20:25 -0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,16 +3,87 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use Faker\Factory as Faker;
+use Illuminate\Support\Arr;
+use App\Models\Login;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $faker = Faker::create('id_ID');
+
+        // ADMIN
+        $token = Str::random(16);
+        $role = "admin";
+        $hashPassword = Hash::make('jancok', [
+            'rounds' => 12,
+        ]);
+        $hashToken = Hash::make($token, [
+            'rounds' => 12,
+        ]);
+        Login::create([
+            'login_nama' => 'Administrator',
+            'login_username' => 'admin',
+            'login_password' => $hashPassword,
+            'login_email' => 'administrator@gmail.com',
+            'login_telepon' => '0855939293192',
+            'login_token' => $hashToken,
+            'login_level' => $role,
+            'login_status' => "verified",
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        // ------------------------------------------------
+        // USER - AWAN
+        $token = Str::random(16);
+        $role = "user";
+        $hashPassword = Hash::make('awan', [
+            'rounds' => 12,
+        ]);
+        $hashToken = Hash::make($token, [
+            'rounds' => 12,
+        ]);
+        Login::create([
+            'login_nama' => 'awan',
+            'login_username' => 'awan',
+            'login_password' => $hashPassword,
+            'login_email' => 'awan@gmail.com',
+            'login_telepon' => '082493929129',
+            'login_token' => $hashToken,
+            'login_level' => $role,
+            'login_status' => "verified",
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        // ---------------------------------------------------------------------------
+        // ------------------------------------------------
+        // USER - DEFAULT
+        $token = Str::random(16);
+        $role = "user";
+        $hashPassword = Hash::make('usertest', [
+            'rounds' => 12,
+        ]);
+        $hashToken = Hash::make($token, [
+            'rounds' => 12,
+        ]);
+        Login::create([
+            'login_nama' => 'usertest',
+            'login_username' => 'usertest',
+            'login_password' => $hashPassword,
+            'login_email' => 'usertest@gmail.com',
+            'login_telepon' => '082249491021',
+            'login_token' => $hashToken,
+            'login_level' => $role,
+            'login_status' => "verified",
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        // ---------------------------------------------------------------------------
     }
 }
